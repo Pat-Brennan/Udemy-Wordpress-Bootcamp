c4ead8b9b595aabd4cfd2ddb4ed671b5f3f0fe25
Patrick Brennan <patbrennan7@gmail.com>
2022-11-08 15:52:33 -0500

    PHP Functions Covered

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,10 +1,20 @@
-
 <!-- This is how you define a function!  -->
-<?php 
-  function firstFunc() {
-    echo "<p>I am HTML written inside of a Function!</p>";
-  }
+<?php
+function firstFunc()
+{
+  echo "<p>I am HTML written inside of a Function!</p>";
+}
 
-  firstFunc();
-  firstFunc();
+firstFunc();
+firstFunc();
+?>
+
+<?php
+function greet($insult, $name)
+{
+  echo "<p> Greetings $insult! I a $name. Who goes there?! </p>";
+}
+
+greet("Plebian", "Beavis");
+greet("ya big jerk!", "Butthead");
 ?>
