3d28ac8dea217f755998af361896e455d094096d
Patrick Brennan <patbrennan7@gmail.com>
2022-11-08 15:26:04 -0500

    First Commit Ba-Beee!

---
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -0,0 +1,10 @@
+
+<!-- This is how you define a function!  -->
+<?php 
+  function firstFunc() {
+    echo "<p>I am HTML written inside of a Function!</p>";
+  }
+
+  firstFunc();
+  firstFunc();
+?>
