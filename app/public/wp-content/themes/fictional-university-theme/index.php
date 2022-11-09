147ba5845f8b4bc0e3bece29f07ef058c91d2937
Patrick Brennan <patbrennan7@gmail.com>
2022-11-09 13:29:27 -0500

    Single and Page Files Added

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,35 +1,8 @@
-<!-- This is how you define a function!  -->
-<?php
-function firstFunc()
-{
-  echo "<p>I am HTML written inside of a Function!</p>";
-}
-
-firstFunc();
-firstFunc();
-?>
-
-<?php
-function greet($insult, $name)
-{
-  echo "<p> Greetings $insult! I a $name. Who goes there?! </p>";
-}
-
-greet("Plebian", "Beavis");
-greet("ya big jerk!", "Butthead");
-?>
-
-<?php
-
-$names = array('buzz', 'lightyear', 'jerry', 'the bee');
-
-$count = 0;
-
-while ($count < count($names)) {
-  echo "<li>Hi, my name is $names[$count] </li>";
-  $count++;
-}
-
-?>
-
-<p> Howdy! I'm <?php echo $names[0] ?>.</p>
+<?php while (have_posts()) {
+  the_post(); ?>
+<h2><a href="<?php the_permalink() ?>">
+    <?php the_title(); ?>
+  </a></h2>
+<?php the_content() ?>
+<hr>
+<?php } ?>
