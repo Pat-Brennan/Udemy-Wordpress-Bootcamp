7b0f9d2bfa849a2da189361986adbbf749327b96
Patrick Brennan <patbrennan7@gmail.com>
2022-11-16 12:59:34 -0500

    Button Connectivity Started

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -4,11 +4,11 @@ while (have_posts()) {
   the_post(); ?>
 
 <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title() ?></h1>
         <div class="page-banner__intro">
-          <p>Learn how the school of your dreams got started.</p>
+          <p>PLEASE REPLACE ME I WANT TO BE REPLACED</p>
         </div>
       </div>
     </div>
@@ -20,13 +20,13 @@ while (have_posts()) {
         </p>
       </div>
 
-      <div class="page-links">
+      <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
           <li class="current_page_item"><a href="#">Our History</a></li>
           <li><a href="#">Our Goals</a></li>
         </ul>
-      </div>
+      </div> -->
 
       <div class="generic-content">
         <?php the_content() ?>
