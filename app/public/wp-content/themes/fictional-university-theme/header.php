b8cd5ce50f881405f9b6c8ffc70f123c3edc6c58
Patrick Brennan <patbrennan7@gmail.com>
2022-11-16 17:09:05 -0500

    Nav Bar Edited

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
   <head>
+    <meta charset="<?php bloginfo('charset')?>">
+    <meta name="viewport" content="width=device-width, intial-scale=1">
     <?php wp_head(); ?>
   </head>
-  <body>
+  <body <?php body_class(); ?>>
   <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
@@ -14,7 +16,7 @@
         <div class="site-header__menu group">
           <nav class="main-navigation">
             <ul>
-              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+              <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 14) echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="<?php echo site_url('/programs') ?>">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
