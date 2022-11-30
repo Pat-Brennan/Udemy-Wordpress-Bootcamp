40400631f5f6693f256d7e98f78c59dfb92e7f51
Patrick Brennan <patbrennan7@gmail.com>
2022-11-30 18:51:38 -0500

    Functions File Edited

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -15,15 +15,5 @@
 
   add_action('after_setup_theme', 'university_features');
 
-  function university_post_types() {
-    register_post_type('event', array(
-      'public' => true,
-      'menu_icon' => 'dashicons-calendar',
-      'labels' => array(
-        'name' => 'Events'
-      )
-    ));
-  }
 
-  add_action('init', 'university_post_types');
 ?>
